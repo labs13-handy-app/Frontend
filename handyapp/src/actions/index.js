3bdb178569166306f0704b40c115311e8307c185
tjforeman <tylerforeman92@gmail.com>
2019-06-05 19:24:33 -0400

    added action and reducer for stripe connect

--- handyapp/src/actions/index.js
+++ handyapp/src/actions/index.js
@@ -1,3 +1,4 @@
-export {GET_TOKEN_START, GET_TOKEN_SUCCESS, GET_TOKEN_FAILURE} from './types';
+export {GET_TOKEN_START, GET_TOKEN_SUCCESS, GET_TOKEN_FAILURE, PAYMENT_CONNECTION_START, PAYMENT_CONNECTION_SUCCESS, PAYMENT_CONNECTION_FAILURE} from './types';
 
 export {getToken} from './getToken';
+export {paymentConnection} from './paymentConnection';
