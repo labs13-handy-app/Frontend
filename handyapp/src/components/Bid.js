81d4ba293b1bcc9d9189289847d7e734164c6cf4
Spencer Meredith <Smeredith1119@gmail.com>
2019-06-05 14:02:20 -0700

    installed reactstrap bootstrap dependancy to use and test components

---
+++ handyapp/src/components/Bid.js
@@ -0,0 +1,7 @@
+// import { Button } from 'reactstrap';
+
+// export default (props) => {
+//   return (
+//     <Button color="danger">Danger!</Button>
+//   );
+// };
--- handyapp/src/index.js
+++ handyapp/src/index.js
@@ -6,6 +6,7 @@ import {applyMiddleware, createStore, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import logger from 'redux-logger';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
 
