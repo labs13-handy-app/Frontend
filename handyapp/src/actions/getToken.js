42295c727146824c03690fdc452f20d469b8a5c9
guillsav <guillsav@gmail.com>
2019-06-06 08:36:17 -0400

    fixing token bug

--- handyapp/src/actions/getToken.js
+++ handyapp/src/actions/getToken.js
@@ -1,12 +1,13 @@
 import axiosWithAuth from '../utils/AxiosAuth';
 import {GET_TOKEN_START, GET_TOKEN_SUCCESS, GET_TOKEN_FAILURE} from './index';
 
-export const getToken = () => dispatch => {
+export const getToken = token => dispatch => {
   dispatch({type: GET_TOKEN_START});
-  return axiosWithAuth()
-    .post('https://handy-app-api.herokuapp.com/register')
+  axiosWithAuth()
+    .post('https:/localhost:5000/register')
     .then(res => {
       dispatch({type: GET_TOKEN_SUCCESS, payload: res.data});
+      localStorage.setItem('token', token);
     })
     .catch(err => dispatch({type: GET_TOKEN_FAILURE, payload: err.message}));
 };
--- handyapp/src/components/Auth/Auth.js
+++ handyapp/src/components/Auth/Auth.js
@@ -6,7 +6,6 @@ export default class Auth {
   accessToken;
   idToken;
   expiresAt;
-  getProfile;
 
   auth0 = new auth0.WebAuth({
     domain: AUTH_CONFIG.domain,
@@ -26,15 +25,11 @@ export default class Auth {
     this.getAccessToken = this.getAccessToken.bind(this);
     this.getIdToken = this.getIdToken.bind(this);
     this.renewSession = this.renewSession.bind(this);
-    this.getProfile = this.getProfile.bind(this);
-    this.getExpiryDate = this.getExpiryDate.bind(this);
-    this.scheduleRenewal();
   }
 
   login() {
     this.auth0.authorize();
   }
-
   handleAuthentication() {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
@@ -66,9 +61,6 @@ export default class Auth {
     this.idToken = authResult.idToken;
     this.expiresAt = expiresAt;
 
-    // schedule a token renewal
-    this.scheduleRenewal();
-
     // navigate to the home route
     history.replace('/home');
   }
@@ -87,30 +79,14 @@ export default class Auth {
     });
   }
 
-  getProfile(cb) {
-    this.auth0.client.userInfo(this.accessToken, (err, profile) => {
-      if (profile) {
-        this.userProfile = profile;
-      }
-      cb(err, profile);
-    });
-  }
-
   logout() {
     // Remove tokens and expiry time
     this.accessToken = null;
     this.idToken = null;
     this.expiresAt = 0;
 
-    // Remove user profile
-    this.userProfile = null;
-
-    // Clear token renewal
-    clearTimeout(this.tokenRenewalTimeout);
-
     // Remove isLoggedIn flag from localStorage
     localStorage.removeItem('isLoggedIn');
-    localStorage.removeItem('token');
 
     this.auth0.logout({
       returnTo: window.location.origin
@@ -125,19 +101,5 @@ export default class Auth {
     // access token's expiry time
     let expiresAt = this.expiresAt;
     return new Date().getTime() < expiresAt;
-  }
-
-  scheduleRenewal() {
-    let expiresAt = this.expiresAt;
-    const timeout = expiresAt - Date.now();
-    if (timeout > 0) {
-      this.tokenRenewalTimeout = setTimeout(() => {
-        this.renewSession();
-      }, timeout);
-    }
-  }
-
-  getExpiryDate() {
-    return JSON.stringify(new Date(this.expiresAt));
   }
 }
