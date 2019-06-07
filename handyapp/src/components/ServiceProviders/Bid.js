c620dd9c38214cb5caebefe911ab21ce8b101e0a
Spencer Meredith <Smeredith1119@gmail.com>
2019-06-07 01:41:14 -0700

    Working on Bid Component

--- handyapp/src/components/ServiceProviders/Bid.js
+++ handyapp/src/components/ServiceProviders/Bid.js
@@ -1,7 +1,112 @@
-// import { Button } from 'reactstrap';
+import React from 'react';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-// export default (props) => {
-//   return (
-//     <Button color="danger">Danger!</Button>
-//   );
-// };
+export default class Example extends React.Component {
+  render() {
+    return (
+      <Form>
+        <FormGroup row>
+          <Label for="Projectid" sm={2}>Project Id</Label>
+          <Col sm={10}>
+            <Input type="text" name="Projectid" id="exampleEmail" placeholder="with a placeholder" />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="ContracterID" sm={2}>Contracter ID</Label>
+          <Col sm={10}>
+            <Input type="text" name="text" id="ContracterID" placeholder="text placeholder" />
+          </Col>
+        </FormGroup>
+        <FormGroup row>a
+          <Label for="exampleSelect" sm={2}>Select</Label>
+          <Col sm={10}>
+            <Input type="select" name="select" id="exampleSelect" />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="exampleSelectMulti" sm={2}>Select Multiple</Label>
+          <Col sm={10}>
+            <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="exampleText" sm={2}>Text Area</Label>
+          <Col sm={10}>
+            <Input type="textarea" name="text" id="exampleText" />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="exampleFile" sm={2}>File</Label>
+          <Col sm={10}>
+            <Input type="file" name="file" id="exampleFile" />
+            <FormText color="muted">
+              This is some placeholder block-level help text for the above input.
+              It's a bit lighter and easily wraps to a new line.
+            </FormText>
+          </Col>
+        </FormGroup>
+        <FormGroup tag="fieldset" row>
+          <legend className="col-form-label col-sm-2">Radio Buttons</legend>
+          <Col sm={10}>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio2" />{' '}
+                Option one is this and that—be sure to include why it's great
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio2" />{' '}
+                Option two can be something else and selecting it will deselect option one
+              </Label>
+            </FormGroup>
+            <FormGroup check disabled>
+              <Label check>
+                <Input type="radio" name="radio2" disabled />{' '}
+                Option three is disabled
+              </Label>
+            </FormGroup>
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="checkbox2" sm={2}>Checkbox</Label>
+          <Col sm={{ size: 10 }}>
+            <FormGroup check>
+              <Label check>
+                <Input type="checkbox" id="checkbox2" />{' '}
+                Check me out
+              </Label>
+            </FormGroup>
+          </Col>
+        </FormGroup>
+        <FormGroup check row>
+          <Col sm={{ size: 10, offset: 2 }}>
+            <Button>Send Bid</Button>
+          </Col>
+        </FormGroup>
+      </Form>
+    );
+  }
+}
+
+
+
+// type text
+// typex number
+
+
+
+
+
+Tags project maneul exist or throw err
+// Project Id
+// Contracter Id
+// Price numbers interggers 
+
+// '' strings ammount of time its going to take 2week 3 days input bar
+//  matrieals included drop down valye yes or no. radio button/ drop down
+
+// send bid button says (button)
+// playground test cloundary
+// profile fixed
+// feedback contracter 
