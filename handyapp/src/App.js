b351e220b4a8aac1965e07b5c7d20260273c2c98
Sharanjit Sandhu <sharanjit.sandhuu@gmail.com>
2019-06-12 00:22:56 -0700

    Created Bottom Nav

--- handyapp/src/App.js
+++ handyapp/src/App.js
@@ -16,7 +16,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
 
-import './App.css';
+//import './App.css';
 
 library.add(fab, faCheckSquare);
 
