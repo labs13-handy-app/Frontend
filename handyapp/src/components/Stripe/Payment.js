1e94e92cada1a876cb307f9b473c75934dab2276
tjforeman <tylerforeman92@gmail.com>
2019-06-19 17:28:36 -0400

    charge working locally

---
+++ handyapp/src/components/Stripe/Payment.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+class Payment extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            stripeEmail:props.user.email,
+            amount:props.user.balance,
+            customer:props.user.stripe_id
+            
+        }
+    }
+
+
+    render(){
+        return(
+            <div>
+            <button onClick={() => this.props.newPayment(this.state)}>
+                Confrim Payment
+            </button>
+            </div>
+        )
+    }
+}
+
+export default Payment
