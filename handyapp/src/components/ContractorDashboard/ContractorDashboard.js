085fb80b07aa85d20d1080cb48fb3bd3e3ce6189
guillsav <guillsav@gmail.com>
2019-06-21 16:45:08 -0400

    merge conflict resolved

--- handyapp/src/components/ContractorDashboard/ContractorDashboard.js
+++ handyapp/src/components/ContractorDashboard/ContractorDashboard.js
@@ -18,12 +18,8 @@ class ContractorDashboard extends Component {
   }
   render() {
     if (!localStorage.isBoarded) {
-<<<<<<< HEAD
-      this.props.history.push('/onboarding-contractor');
-=======
       this.props.history.push('/contractor-onboarding');
       // window.location.reload()
->>>>>>> master
     }
     if (
       !localStorage.account_type &&
