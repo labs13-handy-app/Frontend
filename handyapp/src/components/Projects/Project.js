fcf1d97dd8609df8574411de60085f0996e4303d
guillsav <guillsav@gmail.com>
2019-06-22 14:51:42 -0400

    contractor projectList style done

--- handyapp/src/components/Projects/Project.js
+++ handyapp/src/components/Projects/Project.js
@@ -1,39 +1,76 @@
 import React from 'react';
 import placeholder from '../../img/Placeholder-image.png';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import {Gallery, GalleryImage} from 'react-gesture-gallery';
 
-const Project = props => {
-  console.log(props);
-  if (!localStorage.token) {
-    props.history.push('/');
+import './Project.css';
+
+class Project extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: this.props.images ? this.props.images.map(image => image) : '',
+      index: 0
+    };
   }
-  return (
-    <div className="Project">
-      <div className="project-image">
-        <img src={props.thumbnail ? props.thumbnail : placeholder} alt="" />
-      </div>
-      <div className="project-content">
-        <div className="project-info">
-          <h2>{props.title}</h2>
-          <p>
-            <i className="fas fa-tools" />
-            {props.materials_included === 'yes'
-              ? 'Materials Included'
-              : 'No materials included'}
-          </p>
-          <p>
-            <i className="fas fa-gavel" />5 Bids
-          </p>
+
+  render() {
+    if (!localStorage.token) {
+      this.props.history.push('/');
+    }
+    return (
+      <div className="ContractorProject">
+        <div className="contractor-project-image">
+          <Gallery
+            index={this.state.index}
+            onRequestChange={i => {
+              this.setState({index: i});
+            }}
+          >
+            {this.state.images.map(image => (
+              <GalleryImage
+                key={image}
+                className="carousel"
+                objectFit="cover"
+                src={image}
+              />
+            ))}
+          </Gallery>
         </div>
-        <div className="description">
-          <p>{props.description} </p>
+        <div className="contractor-project-content">
+          <div className="contractor-project-info">
+            <h2>{this.props.title}</h2>
+            <div className="c-info">
+              <p>
+                <i className="fas fa-tools" />
+                {this.props.materials_included === 'yes'
+                  ? 'Materials Included'
+                  : 'No materials included'}
+              </p>
+              <p>
+                <i className="fas fa-gavel" />
+                <span>
+                  {this.props.bids && this.props.bids.length > 0
+                    ? `${this.props.bids.length + 1} Bids `
+                    : `0 Bids `}
+                </span>
+              </p>
+            </div>
+
+            <div className="contractor-description">
+              <p>{this.props.description} </p>
+            </div>
+            <Link to={`/add-bid/${this.props.id}`}>
+              <button className="place-bid">
+                <i className="fas fa-plus" />
+                Place Bid
+              </button>
+            </Link>
+          </div>
         </div>
-        <Link to= {`/add-bid/${props.id}`}>
-        <button>Place Bid</button>
-        </Link>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Project;
