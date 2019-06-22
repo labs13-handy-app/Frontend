0bfc630d708719b4c3bd15996faeeeb901d154ad
guillsav <guillsav@gmail.com>
2019-06-21 23:00:06 -0400

    Carousel on project images done

--- handyapp/src/components/Projects/UserProject.js
+++ handyapp/src/components/Projects/UserProject.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import placeholder from '../../img/Placeholder-image.png';
 import {Link} from 'react-router-dom';
-import {Carousel, CarouselItem} from 'reactstrap';
+import {Gallery, GalleryImage} from 'react-gesture-gallery';
 
 import './UserProject.css';
 
 class UserProject extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: this.props.images
+        ? this.props.images.map(data => data.image)
+        : '',
+      index: 0
+    };
+  }
   render() {
     return (
       <div className="UserProject">
         <div className="user-project-image">
-          <img
-            src={
-              this.props.thumbnail ? this.props.thumbnail.image : placeholder
-            }
-            alt=""
-          />
+          <Gallery
+            index={this.state.index}
+            onRequestChange={i => {
+              this.setState({index: i});
+            }}
+          >
+            {this.state.images.map(image => (
+              <GalleryImage
+                className="carousel"
+                objectFit="cover"
+                src={image}
+              />
+            ))}
+          </Gallery>
         </div>
         <div className="user-project-content">
           <h2>{this.props.title}</h2>
