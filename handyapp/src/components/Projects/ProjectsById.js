8c5595230444294726d159081636e172fb39ea10
guillsav <guillsav@gmail.com>
2019-06-28 00:23:24 -0400

    BidForProject component styled

--- handyapp/src/components/Projects/ProjectsById.js
+++ handyapp/src/components/Projects/ProjectsById.js
@@ -1,30 +1,30 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import BidsForProject from './BidsForProject';
-import {getProjectById, deleteBid} from '../../actions'
+import {getProjectById, deleteBid} from '../../actions';
 import Loader from 'react-loader-spinner';
 
 import './ProjectsById.css';
 
 class ProjectsById extends Component {
-    componentDidMount(){
-    const id=this.props.match.params.id
-      this.props.getProjectById(id);
-    }
-  
-    render() {
-      if (this.props.projects.bids === undefined) {
-        return (
-          <>
-            <Loader type="ThreeDots" color="#4c5b48" height="100" width="100" />
-          </>
-        );
-      } else {
-      console.log(this.props.projects)
-      console.log(this.props.projects.bids)
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.props.getProjectById(id);
+  }
+
+  render() {
+    if (this.props.projects.bids === undefined) {
+      return (
+        <>
+          <Loader type="ThreeDots" color="#4c5b48" height="100" width="100" />
+        </>
+      );
+    } else {
+      console.log(this.props.projects);
+      console.log(this.props.projects.bids);
       return (
         <div className="project-container">
-        {/* <div className='Project'>
+          {/* <div className='Project'>
          <div className="project-image">
         <img src={this.props.thumbnail ? this.props.thumbnail : ''} alt="" />
       </div>
@@ -38,23 +38,28 @@ class ProjectsById extends Component {
           </div>
           </div>
          </div> */}
-           <h2>Bids:</h2>
-           <div className='bid-container'>
-           {this.props.projects.bids.map(bid => (
-             <BidsForProject bid={bid} key={bid.id} deleteBid={this.props.deleteBid} />
-           ))}
-         </div>
+          <h2>List of bids for your project</h2>
+          <div className="bid-container">
+            {this.props.projects.bids.map(bid => (
+              <BidsForProject
+                bid={bid}
+                key={bid.id}
+                deleteBid={this.props.deleteBid}
+              />
+            ))}
+          </div>
         </div>
       );
     }
   }
-  }
-  
+}
 
-
-  const mapStateToProps = ({getProjectByIdReducer}, props) => {
-    return {
-      projects: getProjectByIdReducer.projects,
-    };
+const mapStateToProps = ({getProjectByIdReducer}, props) => {
+  return {
+    projects: getProjectByIdReducer.projects
   };
-export default connect(mapStateToProps,{getProjectById,deleteBid})(ProjectsById);
+};
+export default connect(
+  mapStateToProps,
+  {getProjectById, deleteBid}
+)(ProjectsById);
